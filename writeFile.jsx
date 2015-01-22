50e80afb198839010a0ece6a26df00b6c0df9302
vingojw <vingojw@163.com>
2015-01-22 18:06:48 +0800

    u

--- writeFile.jsx
+++ writeFile.jsx
@@ -3,21 +3,33 @@ app.bringToFront();
 
 //写入任务操作
 var strFile = new File( "e:\\test.txt");
-alert(strFile);
 //if (strFile.exists)
 //{
-    strFile.open ("w");
+    strFile.open ("e");
     //strFile.writeln(app.activeDocument.name);
     //strFile.writeln((nowTime.getMonth() +1).toString() + "月" + nowTime.getDate().toString() + "日");
-    strFile.writeln(1231);
+    strFile.writeln('aaa');
     strFile.close();
 //}
-
+var folder = new Folder ("e:\\testScript");
+folder.create(); //创建文件夹
 /*
-r	只读模式。文件指针被放置在文件的开头。这是默认模式。
-r+	读写模式。文件指针被放置在文件的开头。
-w	只写模式。如果文件存在，则重写文件。如果文件不存在，则创建一个新文件用于写入。
-w+	读写模式。如果文件存在，则重写已存在的文件。如果文件不存在，则创建一个新文件用于读写。
-a	只写模式。如果文件存在，则文件指针被放置在文件的末尾。也就是说，文件是追加模式。如果文件不存在，则创建一个新文件用于写入。
-a+	读写模式。如果文件存在，则文件指针被放置在文件的末尾。也就是说，文件是追加模式。如果文件不存在，则创建一个新文件用于读写。
+
+fileObj.open (mode[,type][,creator])
+mode
+	读
+	r: (read) Opens for reading. If the file does not exist or cannot be found, the call
+	fails.
+
+	写 ，存在就覆盖内容，不存在就新建文件再写
+	w: (write) Opens a file for writing. If the file exists, its contents are destroyed. If
+	the file does not exist, creates a new, empty file.
+
+	读写状态
+	e: (edit) Opens an existing file for reading and writing.
+
+	追加
+	a: (append) Opens the file in Append mode, and moves the current position to
+	the end of the file.
 */
+
